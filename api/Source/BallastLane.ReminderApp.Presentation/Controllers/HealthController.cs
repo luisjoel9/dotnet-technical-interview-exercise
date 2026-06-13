8acb6d1fb85702e10d04c718bbb530e76c1fe846
joel chambilla <joel.luis.cr@gmail.com>
2026-06-13 17:05:00 -0400

    chore(config): Initialize project skeleton

---
+++ api/Source/BallastLane.ReminderApp.Presentation/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BallastLane.ReminderApp.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Api is running...");
+        }
+    }
+}
